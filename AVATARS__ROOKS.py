6a49ff023320faad605e458ab76a03eabd0c45ee
maicolvn1399 <55199282+maicolvn1399@users.noreply.github.com>
2020-07-14 19:58:32 -0600

    se agregan los proyectiles a una de las rooks

--- AVATARS__ROOKS.py
+++ AVATARS__ROOKS.py
@@ -30,12 +30,15 @@ moneyRate = 120
 startingMoneyBooster = 1
 
 #Set up win/lose conditions
-MAX_BAD_REVIEWS = 3#cantidad de avatars que pasan
-WIN_TIME = FRAME_RATE *60 * 3
+ENEMIES_PASSED = 8 #cantidad de avatars que pasan
+WIN_TIME = FRAME_RATE * 60 * 3
 
 #Speeds
 REG_SPEED = 2
 SLOW_SPEED = 1
+
+rock_rook_coordinates = []
+FIRE_RATE = 60
 
 #____________load assets__________________
 
@@ -88,6 +91,15 @@ rock_rook_surf = Surface.convert_alpha(rock_rook)
 ROCK_ROOK = transform.scale(rock_rook_surf,(WIDTH,HEIGHT))
 
 
+#set up bullets
+sand_bullet_img = image.load("sandEffect.png")
+sand_bullet_surf = Surface.convert_alpha(sand_bullet_img)
+SAND_BULLET = transform.scale(sand_rook_surf,(30,30))
+
+
+#image for explosion
+explosion_img = image.load("explosion.png")
+EXPLOSION = explosion_img
 
 #set up classes
 class Avatar_Archer(sprite.Sprite):
@@ -104,6 +116,7 @@ class Avatar_Archer(sprite.Sprite):
         self.walkSeconds = walkSeconds
         self.stop = False
         self.rect = self.image.get_rect(center=(1100,y))
+        self.despawn_wait = None
 
     def update(self,game_window,counters):
         game_window.blit(BACKGROUND,
@@ -118,26 +131,54 @@ class Avatar_Archer(sprite.Sprite):
         else:
             self.stop = False
 
-        if self.health <= 0 or self.rect.x <= 100:
+        isColliding = sprite.spritecollide(self,all_bullets,True)
+        if isColliding is not None:
+            for bullet in isColliding:
+                self.health -= 10
+
+        if self.rect.x <= 100:
+            counters.enemies_passed += 1
+            self.despawn_wait = 0
+
+        if self.despawn_wait is None:
+            if self.health <= 0:
+                self.image = EXPLOSION
+                self.speed = 0
+                self.despawn_wait = 20
+            game_window.blit(self.image,(self.rect.x,self.rect.y))
+        elif self.despawn_wait <= 0:
             self.kill()
         else:
-            game_window.blit(self.image, (self.rect.x, self.rect.y))
+            self.despawn_wait -= 1
+        game_window.blit(self.image,(self.rect.x,self.rect.y))
+
+
+
 
     def attack(self,tile):
         if tile.rook == WATERrook:
             self.speed = SLOW_SPEED
         elif tile.rook == FIRErook:
             self.health -= 1
+        elif tile.rook == ROCKrook:
+            self.health = 0
+
+
 
 
 class Counters(object):
-    def __init__(self,enemy_money, money_rate,money_booster):
+    def __init__(self,enemy_money, money_rate,money_booster,timer,enemies_passed,fire_rate):
         self.loop_count = 0
         self.display_font = font.SysFont(None,25)
         self.enemy_money = enemy_money
         self.money_rate = money_rate
         self.money_booster = money_booster
         self.money_rect = None
+        self.timer = timer
+        self.timer_rect = None
+        self.enemies_passed = 0
+        self.enemies_passed_rect = None
+        self.fire_rate = fire_rate
 
     def increment_money(self):
         if self.loop_count % self.money_rate == 0:
@@ -152,10 +193,41 @@ class Counters(object):
         self.money_rect.y = WINDOW_HEIGHT - 50
         game_window.blit(money_surface,self.money_rect)
 
+    def draw_enemies_passed(self,game_window):
+        if bool(self.enemies_passed_rect):
+            game_window.blit(GAME_WINDOW,
+                             (self.enemies_passed_rect.x,self.enemies_passed_rect.y),self.enemies_passed_rect)
+
+
+            enemies_passed_surf = self.display_font.render(str(self.enemies_passed),True,WHITE)
+            self.enemies_passed_rect = enemies_passed_surf.get_rect()
+
+            self.enemies_passed_rect.x = WINDOW_WIDTH - 150
+            self.enemies_passed_rect.y = WINDOW_HEIGHT - 50
+            game_window.blit(enemies_passed_surf,self.enemies_passed_rect)
+
+    def draw_timer(self,game_window):
+        if bool(self.timer_rect):
+            game_window.blit(game_window,
+                             (self.timer_rect.x, self.timer_rect.y),self.timer_rect)
+            timer_surf = self.display_font.render(str(WIN_TIME - self.loop_count // FRAME_RATE),True,WHITE)
+            self.timer_rect = timer_surf.get_rect()
+            self.timer_rect.x = WINDOW_WIDTH - 250
+            self.timer_rect.y = WINDOW_HEIGHT - 50
+            game_window.blit(timer_surf,self.timer_rect)
+
     def update(self,game_window):
         self.loop_count += 1
         self.increment_money()
         self.draw_money(game_window)
+        self.draw_enemies_passed(game_window)
+        self.draw_timer(game_window)
+        self.update_rock_rook()
+
+    def update_rock_rook(self):
+        for location in rock_rook_coordinates:
+            if self.loop_count%self.fire_rate == 0:
+                Bullets(location)
 
 class Rook(object):
     def __init__(self,type,cost,type_img):
@@ -163,6 +235,9 @@ class Rook(object):
         self.cost = cost
         self.type_img = type_img
         self.rect = self.type_img.get_rect()
+        self.bullet = image.load("waterEffect.png")
+        self.bullet_speed = 1
+        self.bullet_stop = False
 
 class RookApplicator(object):
     def __init__(self):
@@ -171,6 +246,7 @@ class RookApplicator(object):
     def select_rook(self,rook):
         if rook.cost <= counters.enemy_money:
             self.selected = rook
+
 
     def select_tile(self,tile,counters):
         self.selected = tile.set_rook(self.selected,counters)
@@ -189,12 +265,13 @@ class PlayTile(BackgroundTile):
             self.rook = rook
             if rook == SANDrook:
                 counters.money_booster += 1
+            if rook == ROCKrook:
+                rock_rook_coordinates.append((self.rect.x,self.rect.y))
         return None
 
     def draw_rook(self,game_window,rook_applicator):
         if bool(self.rook):
             game_window.blit(self.rook.type_img,(self.rect.x,self.rect.y))
-
 
 class ButtonTile(BackgroundTile):
     def set_rook(self,rook,counters):
@@ -209,6 +286,8 @@ class ButtonTile(BackgroundTile):
                 draw.rect(game_window,(238,190,47),
                           (self.rect.x,self.rect.y,WIDTH,HEIGHT),5)
 
+
+
 class InactiveTiles(BackgroundTile):
     def set_rook(self,rook,counters):
         return None
@@ -216,15 +295,38 @@ class InactiveTiles(BackgroundTile):
     def draw_rook(self,game_window,rook_applicator ):
         pass
 
-#create group for all avatars archers instances
+class Bullets(sprite.Sprite):
+    def __init__(self,coordinates):
+        super().__init__()
+        self.image = SAND_BULLET
+        self.speed = REG_SPEED
+        all_bullets.add(self)
+        self.rect = self.image.get_rect()
+        self.rect.x = coordinates[0] + 40
+        self.rect.y = coordinates[1] + 40
+
+    def update(self,game_window):
+        game_window.blit(BACKGROUND,(self.rect.x,self.rect.y),self.rect)
+
+        self.rect.x += self.speed
+
+        if self.rect.x > 1200:
+            self.kill()
+        else:
+            game_window.blit(self.image,(self.rect.x,self.rect.y))
+
+
+#create group for all avatars instances and bullets instances
 all_avatars_archers = sprite.Group()
+all_bullets = sprite.Group()
 #create an instance of counters
-counters = Counters(startingMoney,moneyRate,startingMoneyBooster)
+counters = Counters(startingMoney,moneyRate,startingMoneyBooster,WIN_TIME,ENEMIES_PASSED,FIRE_RATE)
 
 #rook initialization
 WATERrook = Rook("WATER_ROOK",5,WATER_ROOK)
 FIRErook = Rook("FIRE_ROOK",3,FIRE_ROOK)
 SANDrook = Rook("SAND_ROOK",7,SAND_ROOK)
+ROCKrook = Rook("ROCK_ROOK",8,ROCK_ROOK)
 
 rook_applicator = RookApplicator()
 
@@ -243,15 +345,15 @@ for row in range(6):
             new_tile = InactiveTiles(tile_rect)
         else:
             if row == 5:
-                if 2 <= column <= 4:
+                if 2 <= column <= 5:
                     new_tile = ButtonTile(tile_rect)
-                    new_tile.rook = [WATERrook,FIRErook,SANDrook][column-2]
+                    new_tile.rook = [WATERrook,FIRErook,SANDrook,ROCKrook][column-2]
                 else:
                     new_tile = InactiveTiles(tile_rect)
             else:
                 new_tile = PlayTile(tile_rect)
         row_of_tiles.append(new_tile)
-        if row == 5 and 2 <= column <= 4:
+        if row == 5 and 2 <= column <= 5:
             GAME_WINDOW.blit(new_tile.rook.type_img,
                              (new_tile.rect.x,new_tile.rect.y))
 
@@ -264,6 +366,8 @@ GAME_WINDOW.blit(GAME_WINDOW,(0,0))
 
 #Mainloop
 game_running = True
+program_running = True
+
 
 while game_running:
 
@@ -272,6 +376,7 @@ while game_running:
         #Exit the game
         if event.type == QUIT:
             game_running = False
+            program_running = False
         elif event.type == MOUSEBUTTONDOWN:
             coordinates = mouse.get_pos()
             x = coordinates[0]
@@ -324,6 +429,13 @@ while game_running:
             if right_tile != left_tile:
                 avatar_archer.attack(right_tile)
 
+    #set win or lose conditions
+    if counters.enemies_passed >= ENEMIES_PASSED:
+        game_running = False
+
+    if counters.loop_count > WIN_TIME:
+        game_running = False
+
     #Update enemies
     for avatar in all_avatars_archers:
         avatar.update(GAME_WINDOW,counters)
@@ -334,17 +446,34 @@ while game_running:
         for tile in tile_row:
             tile.draw_rook(GAME_WINDOW,rook_applicator)
 
-
-
+    for bullet in all_bullets:
+        bullet.update(GAME_WINDOW)
 
     counters.update(GAME_WINDOW)
 
     display.update()
     clock.tick(FRAME_RATE)
 
-#End of mainloop
 
+#End of mainloop
 pygame.quit()
+
+end_font = font.SysFont(None,50)
+if program_running:
+    if counters.enemies_passed >= ENEMIES_PASSED:
+        end_surf = end_font.render("GAME OVER",True,WHITE)
+    else:
+        end_surf = end_font.render("YOU WIN!!",True,WHITE)
+        GAME_WINDOW.blit(end_surf,(350,200))
+        display.update()
+
+while program_running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            program_running = False
+    clock.tick(FRAME_RATE)
+
+
 
 
 
